3c621f3b8dc6572207506971c0dbc519fc9be28f
David Boedigheimer <stealth259@gmail.com>
2019-09-12 11:19:38 -0700

    Add uppercase method.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("form#scream-form").submit(function(event) {
     var shoutInput = $("input#shoutInput").val();
 
-    $(".Shout").text(shoutInput);
+    $(".Shout").text(shoutInput.toUpperCase());
 
     $("#scream").show();
 
